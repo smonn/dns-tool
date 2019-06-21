430f7201e76a314c18f239f835de7358ca00fa63
Simon Ingeson <44818+smonn@users.noreply.github.com>
2019-06-21 14:30:01 -0400

    make handler async, improve error handling

--- src/lambda/lookup.ts
+++ src/lambda/lookup.ts
@@ -1,72 +1,93 @@
-import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda"
-import { Resolver } from 'dns';
+import 'regenerator-runtime/runtime';
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+import { Resolver, MxRecord, NaptrRecord, SoaRecord, SrvRecord, AnyRecord } from 'dns';
 
 export type RRType = 'A' | 'AAAA' | 'ANY' | 'CNAME' | 'MX' | 'NAPTR' | 'NS' | 'PTR' | 'SOA' | 'SRV' | 'TXT';
 
-export type LookupRequest = {
-  servers: string[];
-  hostname: string;
-  type: RRType;
+export interface Headers {
+  [key: string]: string;
 }
 
-/*
-'A'	IPv4 addresses (default)	<string>	dns.resolve4()
-'AAAA'	IPv6 addresses	<string>	dns.resolve6()
-'ANY'	any records	<Object>	dns.resolveAny()
-'CNAME'	canonical name records	<string>	dns.resolveCname()
-'MX'	mail exchange records	<Object>	dns.resolveMx()
-'NAPTR'	name authority pointer records	<Object>	dns.resolveNaptr()
-'NS'	name server records	<string>	dns.resolveNs()
-'PTR'	pointer records	<string>	dns.resolvePtr()
-'SOA'	start of authority records	<Object>	dns.resolveSoa()
-'SRV'	service records	<Object>	dns.resolveSrv()
-'TXT'	text records	<string[]>	dns.resolveTxt()
-*/
+export type LookupRequest = {
+  servers?: string[];
+  hostname?: string;
+  type?: RRType;
+}
 
+export type LookupResponse = {
+  statusCode: number;
+  headers?: Headers;
+  body: string;
+}
 
-const handler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+class StatusCodeError extends Error {
+  statusCode: number;
+  headers?: Headers;
+
+  constructor(message: string, statusCode: number, headers?: Headers) {
+    super(message);
+    this.statusCode = statusCode;
+    this.headers = headers;
+  }
+}
+
+export type ResolveResult = string[] | MxRecord[] | NaptrRecord[] | SoaRecord | SrvRecord[] | string[][] | AnyRecord[];
+
+async function resolveAsync(request: LookupRequest): Promise<ResolveResult> {
+  return new Promise((resolve, reject) => {
+    if (!request.servers) {
+      return reject(new StatusCodeError('Missing servers.', 400));
+    }
+
+    if (!request.hostname) {
+      return reject(new StatusCodeError('Missing hostname.', 400));
+    }
+
+    if (!request.type) {
+      return reject(new StatusCodeError('Missing record type.', 400));
+    }
+
+    const resolver = new Resolver();
+
+    resolver.setServers(request.servers);
+
+    resolver.resolve(request.hostname, request.type, (err, addresses) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(addresses);
+    });
+  });
+}
+
+const handler: Handler<APIGatewayEvent, LookupResponse> = async (event) => {
   try {
     if (event.httpMethod !== 'POST') {
-      callback(null, {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Must be a POST' }),
-      });
-      return;
+      throw new StatusCodeError(`HTTP method ${event.httpMethod} not allowed. Must be POST.`, 405, { Allow: 'POST' });
     }
   
     if (!event.body) {
-      callback(null, {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'No request body.' }),
-      });
-      return;
+      throw new StatusCodeError('Missing request body.', 400);
     }
   
     const request: LookupRequest = JSON.parse(event.body);
-    const resolver = new Resolver();
-    resolver.setServers(request.servers);
-    resolver.resolve(request.hostname, request.type, (err, addresses) => {
-      if (err) {
-        callback(null, {
-          statusCode: 500,
-          body: JSON.stringify({ error: err.message }),
-        });
-        return;
-      }
-  
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(addresses),
-      });
-    });
+    const result = await resolveAsync(request);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ result }),
+    };
   } catch (err) {
-    callback(null, {
-      statusCode: 500,
+    return {
+      statusCode: err.statusCode || 500,
+      headers: err.headers || {},
       body: JSON.stringify({
         error: err.message,
       }),
-    });
+    };
   }
 };
 
-export { handler }
+export { handler };
+
