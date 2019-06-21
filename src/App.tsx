f3905d9d1feca45c289e34165d79eee8c3ad1294
Simon Ingeson <44818+smonn@users.noreply.github.com>
2019-06-21 14:29:08 -0400

    add basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, FormEvent, ChangeEvent } from 'react';
 
-const App: React.FC = () => {
+function App() {
+  const [hostname, setHostname] = useState('');
+  const [type, setType] = useState('A');
+  const [result, setResult] = useState<any>(null);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = useMemo(() => {
+    return async (event: FormEvent<HTMLFormElement>) => {
+      try {
+        setSubmitting(true);
+        event.preventDefault();
+        const res = await fetch('/.netlify/functions/lookup', {
+          method: 'POST',
+          body: JSON.stringify({ servers: ['1.1.1.1', '1.0.0.1'], hostname, type })
+        });
+
+        const json = await res.json();
+        setResult(json);
+      } catch (err) {
+        setResult(err.message);
+      } finally {
+        setSubmitting(false);
+      }
+    };
+  }, [hostname, type]);
+
+  const handleChange = useMemo(() => {
+    return (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      if (event.target.name === 'hostname') {
+        setHostname(event.target.value);
+      }
+      if (event.target.name === 'type') {
+        setType(event.target.value);
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>DNS lookup using Cloudflare's DNS servers (<code>1.1.1.1</code> and <code>1.0.0.1</code>). More options and reverse lookup to be added...</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="hostname">Hostname</label>
+        <input disabled={submitting} id="hostname" name="hostname" required value={hostname} onChange={handleChange} />
+        <label htmlFor="type">Type</label>
+        <select id="type" name="type" value={type} onChange={handleChange}>
+          <option>A</option>
+          <option>AAAA</option>
+          <option>CNAME</option>
+          <option>MX</option>
+          <option>NAPTR</option>
+          <option>NS</option>
+          <option>PTR</option>
+          <option>SOA</option>
+          <option>SRV</option>
+          <option>TXT</option>
+        </select>
+        <button disabled={submitting} type="submit">Lookup</button>
+      </form>
+      <pre>{result ? JSON.stringify(result, null, 2) : '--- result will be displayed here'}</pre>
     </div>
   );
 }
---
+++ src/lambda/lookup.ts
@@ -0,0 +1,72 @@
+import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda"
+import { Resolver } from 'dns';
+
+type RRType = 'A' | 'AAAA' | 'ANY' | 'CNAME' | 'MX' | 'NAPTR' | 'NS' | 'PTR' | 'SOA' | 'SRV' | 'TXT';
+
+type LookupRequest = {
+  servers: string[];
+  hostname: string;
+  type: RRType;
+}
+
+/*
+'A'	IPv4 addresses (default)	<string>	dns.resolve4()
+'AAAA'	IPv6 addresses	<string>	dns.resolve6()
+'ANY'	any records	<Object>	dns.resolveAny()
+'CNAME'	canonical name records	<string>	dns.resolveCname()
+'MX'	mail exchange records	<Object>	dns.resolveMx()
+'NAPTR'	name authority pointer records	<Object>	dns.resolveNaptr()
+'NS'	name server records	<string>	dns.resolveNs()
+'PTR'	pointer records	<string>	dns.resolvePtr()
+'SOA'	start of authority records	<Object>	dns.resolveSoa()
+'SRV'	service records	<Object>	dns.resolveSrv()
+'TXT'	text records	<string[]>	dns.resolveTxt()
+*/
+
+
+const handler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  try {
+    if (event.httpMethod !== 'POST') {
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Must be a POST' }),
+      });
+      return;
+    }
+  
+    if (!event.body) {
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'No request body.' }),
+      });
+      return;
+    }
+  
+    const request: LookupRequest = JSON.parse(event.body);
+    const resolver = new Resolver();
+    resolver.setServers(request.servers);
+    resolver.resolve(request.hostname, request.type, (err, addresses) => {
+      if (err) {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({ error: err.message }),
+        });
+        return;
+      }
+  
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(addresses),
+      });
+    });
+  } catch (err) {
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    });
+  }
+};
+
+export { handler }
